1558ce71b18c79fd04cfcc1157eb3afacc825c7f
anas bhih <bhih.anas@gmail.com>
2021-08-03 16:40:14 +0200

    adding project files

---
+++ weCodeBlog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
