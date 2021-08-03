c541289ba9ad4783a1c5b22beb4671b44a574d6c
anas bhih <bhih.anas@gmail.com>
2021-08-03 17:51:39 +0200

    add bootstrap

--- weCodeBlog/urls.py
+++ weCodeBlog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.home, name="home")
+    # path('', views.home, name="home")
+    path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail')
 ]
--- weCodeBlog/views.py
+++ weCodeBlog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+# def home(request):
+#     return render(request, 'home.html', {})
 
-def home(request):
-    return render(request, 'home.html', {})
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_detail.html'
