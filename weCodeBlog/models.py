0a6542aad21fc861d78d0f9f124a869d52ca0aaa
anas bhih <bhih.anas@gmail.com>
2021-08-03 18:28:34 +0200

    migrating database

--- weCodeBlog/models.py
+++ weCodeBlog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="weCode")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
