482c8a9dd8838a8cbe2932aec2f717ab6422d37e
anas bhih <bhih.anas@gmail.com>
2021-08-05 19:44:54 +0200

    create and update date

--- weCodeBlog/models.py
+++ weCodeBlog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
--- weCodeBlog/views.py
+++ weCodeBlog/views.py
@@ -10,6 +10,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-post_date']
 
 class PostDetailView(DetailView):
     model = Post
