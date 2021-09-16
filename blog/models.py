124820d077991db3d64f69276cf1bdc7a5fd86a4
anas bhih <bhih.anas@gmail.com>
2021-09-16 12:25:08 +0200

    add arabic slugify

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 from ckeditor.fields import RichTextField
+from django.db.models.signals import pre_save
+# from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -15,6 +17,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, allow_unicode=True)
     summary = models.CharField(max_length=500)
     header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     tags = models.CharField(max_length=255)
@@ -22,15 +25,42 @@ class Post(models.Model):
     body = RichTextField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
     update_date = models.DateField(auto_now=True)
-    category = models.CharField(max_length=255, default='web developing')
-    visits = models.IntegerField()
-    up_votes = models.IntegerField()
+    category = models.CharField(max_length=255)
+    visits = models.IntegerField(default=0)
+    up_votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
     def get_absolute_url(self):
-        return reverse('post-detail', args=(str(self.id)))
+        return reverse('post-detail', kwargs={"slug":self.slug})
+
+
+def create_slug(instance, new_slug=None):
+    slug = arabic_slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+def arabic_slugify(str):
+    str = str.replace(" ", "-")
+    str = str.replace(",", "-")
+    str = str.replace("(", "-")
+    str = str.replace(")", "")
+    str = str.replace("؟", "")
+    return str
+
+def pre_save_post_reciever(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_reciever, sender=Post)
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,12 @@ from .views import HomeView, PostDetailView, AddPostView, UpdatePostView, Delete
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/addcomment', PostDetailView.as_view(), name='add-comment'),
+    path('post/<str:slug>', PostDetailView.as_view(), name='post-detail'),
+    path('post/<str:slug>/addcomment', PostDetailView.as_view(), name='add-comment'),
     path('add_post/', AddPostView.as_view(), name='add-post'),
     path('add_category/', AddCategoryView.as_view(), name='add-category'),
-    path('update_post/<int:pk>', UpdatePostView.as_view(), name='update-post'),
-    path('post/<int:pk>/delete', DeletePostView.as_view(), name='delete-post'),
+    path('update_post/<str:slug>', UpdatePostView.as_view(), name='update-post'),
+    path('post/<str:slug>/delete', DeletePostView.as_view(), name='delete-post'),
     path('category/<str:cats>/', CategoryView, name='category'),
     path('categories/', CategoryListView, name='categories')
 ]
