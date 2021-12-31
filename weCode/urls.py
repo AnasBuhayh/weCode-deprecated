13328e8bf37c21e6a65c82afe88895556b52238c
Omar Almalol <omar.alma3lol@gmail.com>
2021-12-31 02:06:39 +0200

    Update urls.py

--- weCode/urls.py
+++ weCode/urls.py
@@ -8,7 +8,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+import sys
+
+TESTING = sys.argv[1:2] == ['test']
+if not TESTING:
+    urlpatterns[1:1] = path('', include('blog.urls'))
